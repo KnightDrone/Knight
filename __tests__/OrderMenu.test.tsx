cd213b87ce7e69f163dcc765193cf269670ab1d5
Zak <zakaria.laribi@epfl.ch>
2024-04-06 13:15:56 +0200

    Modified test to match changes to text

--- __tests__/OrderMenu.test.tsx
+++ __tests__/OrderMenu.test.tsx
@@ -13,10 +13,10 @@ describe("Order Menu", () => {
     const { getByText } = render(<OrderMenu />);
 
     expect(getByText("Choose your item")).toBeTruthy();
-    expect(getByText("First Aid Kit")).toBeTruthy();
-    expect(getByText("Thermal Blanket")).toBeTruthy();
+    expect(getByText("First aid kit")).toBeTruthy();
+    expect(getByText("Thermal blanket")).toBeTruthy();
     expect(getByText("Flashlight")).toBeTruthy();
-    expect(getByText("Power Bank")).toBeTruthy();
+    expect(getByText("Power bank")).toBeTruthy();
   });
 
   it("handles button presses", () => {
@@ -24,21 +24,21 @@ describe("Order Menu", () => {
     const consoleSpy = jest.spyOn(console, "log");
     const { getByText } = render(<OrderMenu />);
 
-    const firstAidButton = getByText("First Aid Kit");
+    const firstAidButton = getByText("First aid kit");
     fireEvent.press(firstAidButton);
-    expect(consoleSpy).toHaveBeenCalledWith("First Aid Kit");
+    expect(consoleSpy).toHaveBeenCalledWith("First aid kit");
 
-    const thermalBlanketButton = getByText("Thermal Blanket");
+    const thermalBlanketButton = getByText("Thermal blanket");
     fireEvent.press(thermalBlanketButton);
-    expect(consoleSpy).toHaveBeenCalledWith("Thermal Blanket");
+    expect(consoleSpy).toHaveBeenCalledWith("Thermal blanket");
 
     const flashlightButton = getByText("Flashlight");
     fireEvent.press(flashlightButton);
     expect(consoleSpy).toHaveBeenCalledWith("Flashlight");
 
-    const powerBankButton = getByText("Power Bank");
+    const powerBankButton = getByText("Power bank");
     fireEvent.press(powerBankButton);
-    expect(consoleSpy).toHaveBeenCalledWith("Power Bank");
+    expect(consoleSpy).toHaveBeenCalledWith("Power bank");
 
     // Clean up the mock to ensure it doesn't affect other tests
     consoleSpy.mockRestore();
