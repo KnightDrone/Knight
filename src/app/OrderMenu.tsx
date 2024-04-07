ce462948b7c84cc89fa7c56d6dfdfde0c65b9bfb
Zak <zakaria.laribi@epfl.ch>
2024-04-07 13:40:21 +0200

    Made the triangle background its own component

--- src/app/OrderMenu.tsx
+++ src/app/OrderMenu.tsx
@@ -3,6 +3,7 @@ import OrderButton from "../components/OrderButton";
 import { Text, StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
 import KaiseiRegular from "../../assets/fonts/KaiseiDecol-Regular.ttf";
+import TriangleBackground from "../components/TriangleBackground";
 
 interface OrderProps {
   // Define your component props here
@@ -52,7 +53,7 @@ export default function OrderMenu() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.triangle}></View>
+      <TriangleBackground />
       <Text style={styles.text}>Choose your item</Text>
       {productButtons.map((button) => (
         <OrderButton
@@ -73,24 +74,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "column",
-  },
-  triangle: {
-    width: 0,
-    height: 0,
-    backgroundColor: "transparent",
-    borderStyle: "solid",
-    borderTopWidth: 0,
-    borderRightWidth: 460, // Width of the triangle
-    borderBottomWidth: 750, // Height of the triangle
-    borderLeftWidth: 0, // Width of the triangle
-    borderTopColor: "transparent",
-    borderRightColor: "transparent",
-    borderBottomColor: "#93D39D", // Color of the triangle
-    opacity: 0.35,
-    borderLeftColor: "transparent",
-    position: "absolute", // This ensures the triangle is in the background
-    bottom: -123,
-    left: -23,
   },
   text: {
     fontSize: 36,
