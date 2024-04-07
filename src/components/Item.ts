4d244be6c77fc8ab222bb9741144a053c811e50e
Zak <zakaria.laribi@epfl.ch>
2024-04-07 15:52:57 +0200

    Created Item and Order objects

---
+++ src/components/Item.ts
@@ -0,0 +1,14 @@
+class Item {
+    public name: string;
+    public description: string;
+    public image: string;
+    public price: number;
+
+
+    constructor(name: string, description: string, image: string, price: number) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+}
