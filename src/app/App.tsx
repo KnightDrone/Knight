a73859cbf417ed6f58407ab1f069df7ee2fd3434
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-09 01:09:43 +0200

    fixing font loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import OrderMenu from "./OrderMenu";
+import { useFonts } from "expo-font";
+import KaiseiRegular from "../../assets/fonts/KaiseiDecol-Regular.ttf";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Kaisei-Regular": KaiseiRegular,
+  });
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
