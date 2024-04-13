e1cd2cd93af3f6508202b76a8d002749964cbe52
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-13 21:23:25 +0200

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ const { defaults: tsjPreset } = require("ts-jest/presets");
 module.exports = {
   ...tsjPreset,
   preset: "jest-expo",
+  setupFiles: ["<rootDir>/jestSetupFile.js"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transform: {
     "^.+\\.jsx$": "babel-jest",
     "^.+\\.tsx?$": [
@@ -21,4 +23,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(ttf|png|jpg)$": "<rootDir>/jestAssetTransformer.js",
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native|@react-native|@react-navigation|@firebase|firebase|@react-native-async-storage)",
+  ],
 };
