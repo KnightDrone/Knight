81a2f399d3c7c94eda17330f9f8e6b7397f91d07
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-13 16:42:26 +0200

    improved order.test.ts coverage

--- __tests__/Order.test.ts
+++ __tests__/Order.test.ts
@@ -22,11 +22,11 @@ describe("Order", () => {
   it("should have the correct properties", () => {
     expect(order.getUser()).toBe(user);
     expect(order.getItem()).toBe(item);
-    expect(order.getOrderDate().getTime()).toBeCloseTo(orderDate.getTime(), -1);
+    expect(order.getOrderDate().getTime()).toBeCloseTo(orderDate.getTime(), -2);
     expect(order.getStatus()).toBe(status);
     expect(order.getDeliveryDate().getTime()).toBeCloseTo(
       deliveryDate.getTime(),
-      -1
+      -2
     );
     expect(order.getLocation()).toEqual(location);
   });
@@ -38,5 +38,20 @@ describe("Order", () => {
       Delivered: "Delivered",
       Cancelled: "Cancelled",
     });
+  });
+
+  it("getStatus should return the correct status", () => {
+    expect(order.getStatus()).toBe(OrderStatus.Pending);
+  });
+    
+  it("getDeliveryDate should return the correct delivery date", () => {
+    expect(order.getDeliveryDate().getTime()).toBeCloseTo(
+      deliveryDate.getTime(),
+      -2
+    );
+  });
+
+  it("getLocation should return the correct location", () => {
+    expect(order.getLocation()).toEqual(location);
   });
 });
