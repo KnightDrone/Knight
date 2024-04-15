cf4fbe207be66cd9996bfdb4c70e24a43ce850e7
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-15 11:54:04 +0200

    updated configs for googleauthrequest in signup and login

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -22,22 +22,15 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 // Navigation imports
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import GoogleAuthConfig from "../types/GoogleAuthConfig";
 
 export default function Login({ navigation }: any) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    iosClientId:
-      "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
-    androidClientId:
-      "983400403511-i43set67i4o1e3kb7fl91vrh9r6aemcb.apps.googleusercontent.com",
-    webClientId:
-      "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
-    redirectUri:
-      "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
-  });
+  const [request, response, promptAsync] =
+    Google.useAuthRequest(GoogleAuthConfig);
 
   useEffect(() => {
     if (response?.type === "success") {
