35438c3d8376765ec637b9cf2f68d7d74435dcc1
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-15 11:52:45 +0200

    created a new androidclientd and moved all ids to a file

---
+++ src/types/GoogleAuthConfig.ts
@@ -0,0 +1,13 @@
+const GoogleAuthConfig = {
+  iosClientId:
+    "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
+  androidClientId:
+    "983400403511-467t4ccjvp59l78v073svkflfus76ltt.apps.googleusercontent.com",
+  webClientId:
+    "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
+  // Find a way to make it work for iOS and Android at the same time because the line below makes it work for only iOS
+  //   redirectUri:
+  //     "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
+};
+
+export default GoogleAuthConfig;
