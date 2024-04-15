0b661c3351987ddb1d7bab6cd4613b235aec7700
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-15 12:06:58 +0200

    updated google config file

--- src/types/GoogleAuthConfig.ts
+++ src/types/GoogleAuthConfig.ts
@@ -1,13 +1,36 @@
-const GoogleAuthConfig = {
+type GoogleAuthConfig = {
+  iosClientId?: string;
+  androidClientId?: string;
+  webClientId?: string;
+  redirectUri?: string;
+};
+
+const all: GoogleAuthConfig = {
   iosClientId:
     "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
   androidClientId:
     "983400403511-467t4ccjvp59l78v073svkflfus76ltt.apps.googleusercontent.com",
   webClientId:
     "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
-  // Find a way to make it work for iOS and Android at the same time because the line below makes it work for only iOS
-  //   redirectUri:
-  //     "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
+  redirectUri:
+    "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
 };
 
-export default GoogleAuthConfig;
+const ios: GoogleAuthConfig = {
+  iosClientId:
+    "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
+  redirectUri:
+    "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
+};
+
+const android: GoogleAuthConfig = {
+  androidClientId:
+    "983400403511-467t4ccjvp59l78v073svkflfus76ltt.apps.googleusercontent.com",
+};
+
+const web: GoogleAuthConfig = {
+  webClientId:
+    "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
+};
+
+export default { all, ios, android, web };
