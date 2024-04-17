d1a379383e823c241b701517c2ef1057c2454462
Tiberiu <musattiberiu@gmail.com>
2024-04-17 18:58:42 +0200

    primary color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff9fc",
+          100: "#d8eff5",
+          200: "#b5deec",
+          300: "#82c7de",
+          400: "#48a6c9",
+          500: "#2c89ae",
+          600: "#276f93",
+          700: "#265b78",
+          800: "#274d63",
+          900: "#244155",
+          950: "#132939",
+        },
+      },
+    },
   },
   plugins: [],
 };
