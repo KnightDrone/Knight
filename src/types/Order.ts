3cb453eb308b6c2abbff33d697409b6e3d5728e6
Zak <zakaria.laribi@epfl.ch>
2024-04-17 16:17:23 +0200

    created Location interface

--- src/types/Order.ts
+++ src/types/Order.ts
@@ -8,45 +8,30 @@ enum OrderStatus {
   Cancelled = "Cancelled",
 }
 
-interface OrderLocation {
-  latitude: number;
-  longitude: number;
+interface Location {
+  latitude: number,
+  longitude: number
 }
 
 class Order {
-  private id: string;
   private user: string;
   private item: Item;
   private orderDate: Date;
   private status: OrderStatus;
   private deliveryDate: Date;
-  private location: OrderLocation;
-  private operator: string;
-  private operatorLoc: OrderLocation;
+  private location: Location;
 
   constructor(
     user: string,
     item: Item,
-    location: OrderLocation,
-    orderDate?: Date,
-    deliveryDate?: Date,
-    operator?: string,
-    op_location?: OrderLocation,
-    id?: string
+    location: Location
   ) {
     this.id = id || uuid.v4().toString();
     this.user = user;
     this.item = item;
     this.status = OrderStatus.Pending;
-    this.orderDate = orderDate || new Date();
-    this.deliveryDate = deliveryDate || new Date();
+    this.deliveryDate = new Date();
     this.location = location;
-    this.operator = operator || "";
-    this.operatorLoc = op_location || { latitude: -999, longitude: -999 };
-  }
-
-  getId(): string {
-    return this.id;
   }
 
   getUser(): string {
@@ -69,7 +54,7 @@ class Order {
     return this.deliveryDate;
   }
 
-  getOrderLocation(): OrderLocation {
+  getLocation(): Location {
     return this.location;
   }
 
@@ -107,51 +92,5 @@ class Order {
   }
 }
 
-const orderConverter = {
-  toFirestore: (order: Order) => {
-    return {
-      user: order.getUser(),
-      operator: order.getOpName(),
-      item: order.getItem().toDict(),
-      orderDate: order.getOrderDate(),
-      status: order.getStatus(),
-      deliveryDate: order.getDeliveryDate(),
-      location: {
-        latitude: order.getOrderLocation().latitude,
-        longitude: order.getOrderLocation().longitude,
-      },
-      op_location: {
-        latitude: order.getOperatorLocation().latitude,
-        longitude: order.getOperatorLocation().longitude,
-      },
-    };
-  },
-  fromFirestore: (snapshot: any) => {
-    const data = snapshot.data();
-    const id = snapshot.id;
 
-    const item = new Item(
-      data.item.id,
-      data.item.name,
-      data.item.description,
-      data.item.price
-    );
-    const order = new Order(
-      data.user,
-      item,
-      { latitude: data.location.latitude, longitude: data.location.longitude },
-      new Date(data.orderDate.seconds * 1000),
-      new Date(data.deliveryDate.seconds * 1000),
-      data.operator,
-      {
-        latitude: data.op_location.latitude,
-        longitude: data.op_location.longitude,
-      },
-      id
-    );
-
-    return order;
-  },
-};
-
-export { OrderStatus, OrderLocation, Order, orderConverter };
+export {OrderStatus, Location, Order}
