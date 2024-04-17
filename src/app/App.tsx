c054722edcbadb7e7c2c1d10623c35f44f4cbb38
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-17 15:23:04 +0200

    update MapOverview import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,7 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import ForgotPassword from "./ForgotPassword";
 import OrderMenu from "./OrderMenu";
-import MapView from "./Map";
+import MapOverview from "./Map";
 import OrderPlaced from "./OrderPlaced";
 import "./global.css";
 
@@ -23,7 +23,6 @@ import { useFonts } from "expo-font";
 import KaiseiRegular from "../../assets/fonts/KaiseiDecol-Regular.ttf";
 
 import { registerRootComponent } from "expo";
-import MapOverview from "./Map";
 
 WebBrowser.maybeCompleteAuthSession();
 
