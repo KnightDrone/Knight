7dcf07a7feb222289dfbd5cff721c28a3aba8191
Zak <zakaria.laribi@epfl.ch>
2024-04-20 15:34:49 +0200

    sorted out google auth configs

--- src/types/GoogleAuthConfig.ts
+++ src/types/GoogleAuthConfig.ts
@@ -6,23 +6,23 @@ type GoogleAuthConfig = {
 };
 
 const all: GoogleAuthConfig = {
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID_OAUTH,
+  androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID_OAUTH,
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID_OAUTH,
+  redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI,
 };
 
 const ios: GoogleAuthConfig = {
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID_OAUTH,
+  redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI,
 };
 
 const android: GoogleAuthConfig = {
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
+  androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID_OAUTH,
 };
 
 const web: GoogleAuthConfig = {
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID_OAUTH,
 };
 
 export default { all, ios, android, web };
