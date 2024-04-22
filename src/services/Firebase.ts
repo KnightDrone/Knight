01938dc7e382375d86abb39be7d917a069fbae4d
Zak <zakaria.laribi@epfl.ch>
2024-04-22 07:42:06 +0200

    modified login, signup and forgot password code

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -37,4 +37,4 @@ const authInstance = auth(app);
 const firestoreInstance = firestore(app);
 
 export default app;
-export { authInstance, firestoreInstance };
+export { authInstance, firestoreInstance, auth };
