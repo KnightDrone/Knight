6fcb4277f83aceac0d81c6941ff272d523ea0fad
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-23 19:53:44 +0200

    Update OrderPlaced screen to include arrival time and loading bar

--- src/app/OrderPlaced.tsx
+++ src/app/OrderPlaced.tsx
@@ -1,32 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import "./global.css";
+import { Item } from "../types/Item";
+import { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from "../types/RootStackParamList";
+import { StackNavigationProp } from "@react-navigation/stack";
+import Icon from "react-native-vector-icons/Fontisto";
+import { useFonts } from "expo-font";
+import KaiseiRegular from "../../assets/fonts/KaiseiDecol-Regular.ttf";
+import TriangleBackground from "../components/TriangleBackground";
 
-const OrderPlaced = ({ navigation }: any) => {
+const OrderPlaced = ({
+  route,
+  navigation,
+}: {
+  route: RouteProp<RootStackParamList, "OrderPlaced">;
+  navigation: StackNavigationProp<RootStackParamList, "OrderPlaced">;
+}) => {
+  const [fontsLoaded] = useFonts({
+    "Kaisei-Regular": KaiseiRegular,
+  });
+
+  const { orderedItem, placedAt, userLocation } = route.params;
+
+  const [arrivalTime, setArrivalTime] = useState<number>(0);
+
+  useEffect(() => {
+    const additionalMinutes: number = 1 + Math.random() * 1;
+    const arrivalTime = placedAt.valueOf() + additionalMinutes * 60 * 1000;
+    setArrivalTime(arrivalTime);
+  }, [placedAt]);
+
+  const [completion, setCompletion] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = Date.now();
+      const elapsed = now - placedAt.valueOf();
+      const totalDuration = arrivalTime - placedAt.valueOf();
+      const newCompletion = Math.min(100, (elapsed / totalDuration) * 100);
+
+      setCompletion(newCompletion);
+
+      if (newCompletion >= 100) {
+        clearInterval(interval);
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [placedAt, arrivalTime]);
+
+  const getFormattedArrivalTime = (arrivalDate: Date): string => {
+    const formattedDate: string = arrivalDate.toLocaleString("en-CH", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+
+    return `Arriving at ${formattedDate}`;
+  };
+
   return (
     <View
-      className="h-full p-4 justify-center items-center"
+      className="flex w-full h-full p-4 justify-center items-center"
       testID="order-placed-screen"
     >
-      <Text className="text-xl font-bold">Your order has been placed.</Text>
-      <Text className="text-lg my-2">Arriving at 12:15</Text>
-      <View className="my-2">
-        <Text className="text-center">🚁</Text>
+      <TriangleBackground color="#A0D1e4" />
+      <View className="flex w-full flex-col items-center">
+        <Text className=" text-3xl font-bold font-kaisei">
+          Your order is on its way
+        </Text>
+        <View className="my-2 flex justify-start">
+          <Text className="text-lg my-2 font-kaisei">
+            {getFormattedArrivalTime(new Date(arrivalTime))}
+          </Text>
+        </View>
+
+        {/* Loading bar and helicopter icon */}
+        <View className="w-full bg-gray-200 rounded-lg relative">
+          <View
+            className="bg-blue-500 h-4 rounded-lg"
+            style={{ width: `${Math.min(completion, 100)}%` }}
+          ></View>
+          <View
+            style={{
+              position: "absolute",
+              left: `${Math.min(completion, 100)}%`,
+              transform: [{ translateX: -40 }],
+            }}
+          >
+            <Icon name="helicopter-ambulance" size={50} color="#000000" />
+          </View>
+        </View>
+
+        <View
+          className="p-4 rounded-lg my-10  w-11/12 justify-center items-center"
+          style={{ backgroundColor: "#FFFBF1" }}
+        >
+          <Text className="text-2xl font-semibold font-kaisei">
+            Order summary
+          </Text>
+          <Text className="text-xl my-2 font-kaisei">
+            {orderedItem.getName()}
+          </Text>
+          <Text className="text-lg font-kaisei">Location: {userLocation}</Text>
+          <Image
+            className="w-64 h-64 rounded-lg"
+            source={orderedItem.getImage()}
+          />
+        </View>
+
+        <View
+          className="w-11/12 p-4 rounded-lg mt-2 justify-center items-center"
+          style={{
+            opacity: completion >= 100 ? 1 : 0,
+          }}
+        >
+          <Text className="text-lg font-semibold font-kaisei">
+            Your order should have been delivered!
+          </Text>
+          <Text className="text-lg font-kaisei">Thanks for trusting us!</Text>
+          <TouchableOpacity>
+            <Text className="text-red-500 font-kaisei">Report an issue</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          className="mt-4"
+          onPress={() =>
+            navigation.navigate("OrderHistory", { opOrders: true })
+          }
+        >
+          <Text className="text-black font-kaisei underline">
+            View order history
+          </Text>
+        </TouchableOpacity>
       </View>
-      <View className="bg-gray-500 p-4 rounded-lg my-4">
-        <Text className="text-lg font-semibold">Order summary</Text>
-        <Text className="text-lg my-2">First aid kit</Text>
-        <Text className="text-lg">Location: 46.5° N, 6.63° E</Text>
-      </View>
-      <Image
-        className="w-full h-64 object-cover rounded-lg"
-        source={{
-          uri: "https://images.unsplash.com/photo-1606786322464-1e1e6e3c6f0e",
-        }}
-      />
-      <TouchableOpacity className="mt-4">
-        <Text className="text-blue-500">View order history</Text>
-      </TouchableOpacity>
     </View>
   );
 };
