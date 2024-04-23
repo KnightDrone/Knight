d483477a66198d87f2fdb4fee7d60b2a2487720a
Zak <zakaria.laribi@epfl.ch>
2024-04-23 17:02:07 +0200

    hunting down syntax errors

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -7,6 +7,10 @@ import {
   signInWithPopup,
   signInWithCredential,
   signInWithEmailAndPassword,
+  User,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
@@ -42,4 +46,8 @@ export {
   analytics,
   signInWithCredential,
   signInWithEmailAndPassword,
+  User,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  createUserWithEmailAndPassword,
 };
