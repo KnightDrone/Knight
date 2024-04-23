b3a5bd7a4a6813fb7658e20a57f51b9b15ac9823
Zak <zakaria.laribi@epfl.ch>
2024-04-23 16:35:08 +0200

    back to firebase

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,12 +1,16 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
-// import firebase from '@react-native-firebase/app';
-import firebase from "@react-native-firebase/app";
-import auth from "@react-native-firebase/auth";
-import firestore from "@react-native-firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithCredential,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,20 +27,21 @@ const firebaseConfig = {
   measurementId: "G-N04Q2KKH95",
 };
 // Initialize Firebase
-var app;
 
-(async () => {
-  if (firebase.apps.length === 0) {
-    app = await firebase.initializeApp(firebaseConfig);
-  } else {
-    app = firebase.app();
-  }
-})();
-
-const authInstance = auth(app);
-const firestoreInstance = firestore(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 // export const auth = getAuth(app);
 export default app;
-export { authInstance, firestoreInstance, auth };
+export {
+  auth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  database,
+  analytics,
+  signInWithCredential,
+  signInWithEmailAndPassword,
+};
