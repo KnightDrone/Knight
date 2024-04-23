8ad4550d982ae6491be54b5f45abde9741cd8a1e
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-23 19:52:09 +0200

    fix firebase issue

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  GoogleAuthProvider,
-  signInWithCredential,
-  signInWithEmailAndPassword,
-  User,
-  onAuthStateChanged,
-  sendPasswordResetEmail,
-  createUserWithEmailAndPassword,
-  getReactNativePersistence,
-  initializeAuth,
-} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { Platform } from "react-native";
+
+const APP_ID =
+  Platform.OS === "ios"
+    ? process.env.EXPO_PUBLIC_FIREBASE_IOS_APP_ID
+    : process.env.EXPO_PUBLIC_FIREBASE_ANDROID_APP_ID;
 
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -31,13 +29,14 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_WEB_APP_ID,
+const firebaseConfig: FirebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -49,23 +48,4 @@ const auth = initializeAuth(app, {
 });
 
 export default app;
-export {
-  auth,
-  GoogleAuthProvider,
-  signInWithCredential,
-  signInWithEmailAndPassword,
-  User,
-  onAuthStateChanged,
-  sendPasswordResetEmail,
-  createUserWithEmailAndPassword,
-  firestore,
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  setDoc,
-  where,
-  Firestore,
-};
+export { auth, GoogleAuthProvider, signInWithPopup, database, firebaseConfig };
