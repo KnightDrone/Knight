114c45c71ff68b2fca5d17659e2d52fffe99d9cb
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-23 19:53:15 +0200

    update orderhistory screen to conform to new changes

--- src/app/OrderHistory.tsx
+++ src/app/OrderHistory.tsx
@@ -10,10 +10,10 @@ import {
 import OrderCard from "../components/OrderCard";
 import { Order, OrderLocation, OrderStatus } from "../types/Order";
 import { Item } from "../types/Item";
-import TriangleBackground, {
-  TriangleBackground2,
-} from "../components/TriangleBackground";
-import { MessageBox } from "../ui/MessageBox";
+import TriangleBackground from "../components/TriangleBackground";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { RootStackParamList } from "../types/RootStackParamList";
+import { RouteProp } from "@react-navigation/native";
 
 /* 
 NOTE: This is a temporary solution to simulate fetching orders from a server. Should be replaced with actual database calls
@@ -71,8 +71,15 @@ const fetchOrdersForUserMock = async (
 };
 
 // TODO: Maybe add some search bar to filter?
-// opOrders is a boolean value that determines whether the user is an operator or not, and fetches the corresponding order history
-const OrderHistory = ({ navigation, userId, opOrders }: any) => {
+
+const OrderHistory = ({
+  route,
+}: {
+  route: RouteProp<RootStackParamList, "OrderHistory">;
+}) => {
+  const { opOrders } = route.params;
+
+  const [userId, setUserId] = useState("");
   const [orders, setOrders] = useState<Order[]>([]);
   const [refreshing, setRefreshing] = useState(false);
   const [error, setError] = useState<Error | null>(null);
@@ -80,6 +87,19 @@ const OrderHistory = ({ navigation, userId, opOrders }: any) => {
   useEffect(() => {
     fetchOrders();
   }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userJSON = await AsyncStorage.getItem("@user");
+      console.log(userJSON);
+    };
+
+    fetchUser();
+
+    return () => {
+      // Cleanup function
+    };
+  }, [refreshing]);
 
   const fetchOrders = async () => {
     setRefreshing(true);
@@ -102,6 +122,7 @@ const OrderHistory = ({ navigation, userId, opOrders }: any) => {
   };
   return (
     <View className="mt-16">
+      <TriangleBackground color="#A0D1e4" />
       <View className="flex-row items-center justify-center">
         <TouchableOpacity className="absolute left-4" testID="menu-button">
           <Image
@@ -120,26 +141,16 @@ const OrderHistory = ({ navigation, userId, opOrders }: any) => {
         </TouchableOpacity>
       </View>
 
-      <TriangleBackground2 />
-      {error ? (
-        <MessageBox
-          message={error.message}
-          style="error"
-          onClose={() => setError(null)}
-          testID="error-box"
-        />
-      ) : (
-        <FlatList
-          data={orders}
-          renderItem={({ item }) => <OrderCard order={item} />}
-          keyExtractor={(item) => item.getId()}
-          onEndReached={fetchOrders}
-          onEndReachedThreshold={0.1}
-          refreshing={refreshing}
-          onRefresh={fetchOrders}
-          testID="orderHistoryFlatList"
-        />
-      )}
+      <FlatList
+        className="mt-4 min-h-full"
+        data={orders}
+        renderItem={({ item }) => <OrderCard order={item} />}
+        keyExtractor={(item) => item.getId()}
+        onEndReached={fetchOrders}
+        onEndReachedThreshold={0.1}
+        refreshing={refreshing}
+        onRefresh={fetchOrders}
+      />
     </View>
   );
 };
