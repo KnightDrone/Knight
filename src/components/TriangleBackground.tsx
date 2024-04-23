ffa8c6fc964f082c13300ff674651983166623ca
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-23 19:51:55 +0200

    update triangle background component: add color prop

--- src/components/TriangleBackground.tsx
+++ src/components/TriangleBackground.tsx
@@ -1,49 +1,29 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-export default function TriangleBackground() {
-  return <View style={styles.triangle}></View>;
-}
+export const TriangleBackground = ({ color = "#93D39D" }) => {
+  const style = StyleSheet.create({
+    triangle: {
+      width: 0,
+      height: 0,
+      backgroundColor: "transparent",
+      borderStyle: "solid",
+      borderTopWidth: 0,
+      borderRightWidth: 460, // Width of the triangle
+      borderBottomWidth: 750, // Height of the triangle
+      borderLeftWidth: 0, // Width of the triangle
+      borderTopColor: "transparent",
+      borderRightColor: "transparent",
+      borderBottomColor: color, // Dynamic color
+      opacity: 0.35,
+      borderLeftColor: "transparent",
+      position: "absolute",
+      bottom: -123,
+      left: -123,
+    },
+  });
 
-export function TriangleBackground2() {
-  return <View style={styles.triangle2}></View>;
-}
+  return <View style={style.triangle}></View>;
+};
 
-const styles = StyleSheet.create({
-  triangle: {
-    width: 0,
-    height: 0,
-    backgroundColor: "transparent",
-    borderStyle: "solid",
-    borderTopWidth: 0,
-    borderRightWidth: 460, // Width of the triangle
-    borderBottomWidth: 750, // Height of the triangle
-    borderLeftWidth: 0, // Width of the triangle
-    borderTopColor: "transparent",
-    borderRightColor: "transparent",
-    borderBottomColor: "#93D39D", // Color of the triangle
-    opacity: 0.35,
-    borderLeftColor: "transparent",
-    position: "absolute", // This ensures the triangle is in the background
-    bottom: -123,
-    left: -23,
-  },
-  triangle2: {
-    width: 0,
-    height: 0,
-    backgroundColor: "transparent",
-    borderStyle: "solid",
-    borderTopWidth: 0,
-    borderRightWidth: 460, // Width of the triangle
-    borderBottomWidth: 750, // Height of the triangle
-    borderLeftWidth: 0, // Width of the triangle
-    borderTopColor: "transparent",
-    borderRightColor: "transparent",
-    borderBottomColor: "#A0D1E4", // Color of the triangle
-    opacity: 0.35,
-    borderLeftColor: "transparent",
-    position: "absolute", // This ensures the triangle is in the background
-    bottom: -350,
-    left: -23,
-  },
-});
+export default TriangleBackground;
