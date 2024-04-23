5c1bf10cd889b45302bc6f1f6b4c8e0e3d2b8f0a
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-23 19:50:54 +0200

    add kaisei font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      fontFamily: {
+        kaisei: ["Kaisei-Regular"],
+      },
       colors: {
         primary: {
           50: "#eff9fc",
