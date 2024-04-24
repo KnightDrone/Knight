b21b5bccffb86f83bfdd4b47c02f4740f87d4665
Zak <zakaria.laribi@epfl.ch>
2024-04-24 15:56:37 +0200

    should use web app id

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -9,7 +9,7 @@ import {
   sendPasswordResetEmail,
   createUserWithEmailAndPassword,
   getReactNativePersistence,
-  initializeAuth,
+  getAuth,
 } from "firebase/auth";
 
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
@@ -26,7 +26,6 @@ import {
   setDoc,
   where,
   Firestore,
-  initializeFirestore,
 } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -35,32 +34,21 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDjeBhSsVZsZnO4gQ2xU9rlRdaJt6K1ZI",
-  authDomain: "swent-g25.firebaseapp.com",
-  projectId: "swent-g25",
-  storageBucket: "swent-g25.appspot.com",
-  messagingSenderId: "983400403511",
-  appId: "1:983400403511:web:04caa117a0e3e7424ae5b2",
-  measurementId: "G-N04Q2KKH95",
-};
-// Initialize Firebase
-
-const app = initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-});
-
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-
-const database = getDatabase(app);
+  appId: process.env.FIREBASE_WEB_APP_ID,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("app: " + app);
 const firestore = getFirestore(app);
+const auth = getAuth(app);
+// const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// });
 
 // Initialize Firebase Authentication and get a reference to the service
 // export const auth = getAuth(app);
@@ -68,7 +56,6 @@ export default app;
 export {
   auth,
   GoogleAuthProvider,
-  database,
   signInWithCredential,
   signInWithEmailAndPassword,
   User,
@@ -85,5 +72,4 @@ export {
   setDoc,
   where,
   Firestore,
-  FirebaseConfig,
 };
