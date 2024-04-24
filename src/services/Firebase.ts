72d5624ad1beb0d5d2b01114825f965297944d82
Zak <zakaria.laribi@epfl.ch>
2024-04-24 15:56:37 +0200

    should use web app id

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -9,7 +9,7 @@ import {
   sendPasswordResetEmail,
   createUserWithEmailAndPassword,
   getReactNativePersistence,
-  initializeAuth,
+  getAuth,
 } from "firebase/auth";
 
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
@@ -27,62 +27,34 @@ import {
   setDoc,
   where,
   Firestore,
-  initializeFirestore,
 } from "firebase/firestore";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const APP_ID =
-  Platform.OS === "ios"
-    ? process.env.FIREBASE_IOS_APP_ID
-    : process.env.FIREBASE_ANDROID_APP_ID;
-
-interface FirebaseConfig {
-  apiKey: any;
-  authDomain: any;
-  projectId: any;
-  storageBucket: any;
-  messagingSenderId: any;
-  appId: any;
-  measurementId: any;
-}
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig: FirebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  appId: process.env.FIREBASE_WEB_APP_ID,
 };
 
 // Initialize Firebase
-
-const app = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-});
-
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-
-const database = getDatabase(app);
+const app = initializeApp(firebaseConfig);
+console.log("app: " + app);
 const firestore = getFirestore(app);
+const auth = getAuth(app);
+// const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// });
 
 export default app;
 export {
   auth,
   GoogleAuthProvider,
-  database,
   signInWithCredential,
   signInWithEmailAndPassword,
   User,
@@ -99,5 +71,4 @@ export {
   setDoc,
   where,
   Firestore,
-  FirebaseConfig,
 };
