44dbd589ebef40dc42aeb87e3276dc42fc1d7ddd
EduardoNeville <eduardoneville82@gmail.com>
2024-04-25 18:07:33 +0200

    changed GoogleAuth again

--- src/types/GoogleAuthConfig.ts
+++ src/types/GoogleAuthConfig.ts
@@ -6,31 +6,31 @@ type GoogleAuthConfig = {
 };
 
 const all: GoogleAuthConfig = {
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  iosClientId:
+    "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
+  androidClientId:
+    "983400403511-467t4ccjvp59l78v073svkflfus76ltt.apps.googleusercontent.com",
+  webClientId:
+    "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
+  redirectUri:
+    "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
 };
 
 const ios: GoogleAuthConfig = {
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  iosClientId:
+    "983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl.apps.googleusercontent.com",
+  redirectUri:
+    "com.googleusercontent.apps.983400403511-gi5mo0akb89fcecaivk4q509c63hrvtl:/oauth2redirect/google",
 };
 
 const android: GoogleAuthConfig = {
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  androidClientId:
+    "983400403511-467t4ccjvp59l78v073svkflfus76ltt.apps.googleusercontent.com",
 };
 
 const web: GoogleAuthConfig = {
-  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
-  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
-  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
-  redirectUri: process.env.REDIRECT_URI,
+  webClientId:
+    "983400403511-ff4ntmj4f9qvmmcc6nqh68tn524bp740.apps.googleusercontent.com",
 };
 
 export default { all, ios, android, web };
