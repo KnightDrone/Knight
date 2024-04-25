80ed966c146ff6ecfc6a7f9dda1fd401722f68a8
EduardoNeville <eduardoneville82@gmail.com>
2024-04-25 17:11:22 +0200

    proper Firebase and Google Auth

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,16 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {
+  getAuth,
   GoogleAuthProvider,
+  signInWithPopup,
   signInWithCredential,
   signInWithEmailAndPassword,
   User,
   onAuthStateChanged,
   sendPasswordResetEmail,
   createUserWithEmailAndPassword,
-  getReactNativePersistence,
-  initializeAuth,
 } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { Platform } from "react-native";
 
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -27,18 +30,31 @@ import {
   Firestore,
 } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const APP_ID =
+  Platform.OS === "ios"
+    ? process.env.FIREBASE_IOS_APP_ID
+    : process.env.FIREBASE_ANDROID_APP_ID;
+
+interface FirebaseConfig {
+  apiKey: any;
+  authDomain: any;
+  projectId: any;
+  storageBucket: any;
+  messagingSenderId: any;
+  appId: any;
+  measurementId: any;
+}
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig: FirebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_WEB_APP_ID,
+  appId: APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -54,20 +70,13 @@ export default app;
 export {
   auth,
   GoogleAuthProvider,
+  signInWithPopup,
+  database,
+  analytics,
   signInWithCredential,
   signInWithEmailAndPassword,
   User,
   onAuthStateChanged,
   sendPasswordResetEmail,
   createUserWithEmailAndPassword,
-  firestore,
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  setDoc,
-  where,
-  Firestore,
 };
--- src/types/GoogleAuthConfig.ts
+++ src/types/GoogleAuthConfig.ts
@@ -13,16 +13,24 @@ const all: GoogleAuthConfig = {
 };
 
 const ios: GoogleAuthConfig = {
-  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID_OAUTH,
-  redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI,
+  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
+  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
+  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
+  redirectUri: process.env.REDIRECT_URI,
 };
 
 const android: GoogleAuthConfig = {
-  androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID_OAUTH,
+  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
+  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
+  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
+  redirectUri: process.env.REDIRECT_URI,
 };
 
 const web: GoogleAuthConfig = {
-  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID_OAUTH,
+  iosClientId: process.env.IOS_CLIENT_ID_OAUTH,
+  androidClientId: process.env.ANDROID_CLIENT_ID_OAUTH,
+  webClientId: process.env.WEB_CLIENT_ID_OAUTH,
+  redirectUri: process.env.REDIRECT_URI,
 };
 
 export default { all, ios, android, web };
