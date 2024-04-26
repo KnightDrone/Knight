4fd83f6802f96e8fb2c147082b69890f3da0c766
kalil0321 <kalil.bouzigues@gmail.com>
2024-04-26 19:50:17 +0200

    refactor secureRandom function in random.ts to use expo-crypto for generating secure random numbers

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,5 +1,10 @@
+import * as Crypto from "expo-crypto";
+
 export function secureRandom() {
-  const buffer = new Uint32Array(1); // Create a typed array of 32-bit integers of length 1
-  window.crypto.getRandomValues(buffer); // Populate the array with cryptographically secure random numbers
-  return buffer[0] / (0xffffffff + 1); // Normalize to [0, 1] by dividing by 2^32 - 1
+  const randomBytesBuffer = Crypto.getRandomBytes(4);
+  const dataView = new DataView(randomBytesBuffer.buffer);
+  const num = dataView.getUint32(0);
+
+  // Normalize to [0, 1] by dividing by the maximum possible value (2^32 - 1)
+  return num / (0xffffffff + 1);
 }
