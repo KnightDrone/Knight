7bc56f829c3026c06c32e120405f31e96ec33257
EduardoNeville <eduardoneville82@gmail.com>
2024-04-26 15:40:00 +0200

    fixed issues with App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
-import { User, onAuthStateChanged, auth } from "../services/Firebase";
+import { authInstance } from "../services/Firebase";
+import { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import * as WebBrowser from "expo-web-browser";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 // Imports for Navigation
 import { NavigationContainer } from "@react-navigation/native";
@@ -15,18 +17,9 @@ import ForgotPassword from "./ForgotPassword";
 import OrderMenu from "./OrderMenu";
 import MapOverview from "./Map";
 import OrderPlaced from "./OrderPlaced";
-import DrawerNavigator from "./DrawerNavigation";
 import "./global.css";
 
 import { registerRootComponent } from "expo";
-
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-  DrawerContentComponentProps,
-} from "@react-navigation/drawer";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -42,12 +35,13 @@ type RootStackParamList = {
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
+
 function App() {
   const [fontsLoaded] = useFonts({
     "Kaisei-Regular": KaiseiRegular,
   });
 
-  const [userInfo, setUserInfo] = useState<User | null>(null);
+  const [userInfo, setUserInfo] = useState<FirebaseAuthTypes.User | null>(null);
   const [loading, setLoading] = useState(false);
 
   const checkLocalUser = async () => {
@@ -68,7 +62,7 @@ function App() {
 
   useEffect(() => {
     checkLocalUser();
-    const unsub = onAuthStateChanged(auth, async (user) => {
+    const unsub = authInstance.onAuthStateChanged(async (user) => {
       if (user) {
         setUserInfo(user);
         setIsLoggedIn("Map");
@@ -96,30 +90,82 @@ function App() {
   }
   return (
     <NavigationContainer>
-      {userInfo ? (
-        <DrawerNavigator />
-      ) : (
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{
-            headerShown: false,
-            headerStyle: {
-              backgroundColor: "#f9f9f9",
-            },
-            headerTintColor: "#000",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
+      <Stack.Navigator
+        initialRouteName={userInfo ? "Map" : "Login"}
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
+            backgroundColor: "#f9f9f9",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Login" options={{ title: "Login to Wild Knight" }}>
+          {(props) => <Login {...props} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name="SignUp"
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTransparent: true,
+            headerTitle: "",
+            headerLeft: () => (
+              <HeaderBackButton
+                onPress={() => navigation.goBack()}
+                labelVisible={false}
+                testID="sign-up-back-button"
+              />
+            ),
+          })}
         >
-          <Stack.Screen
-            name="Login"
-            options={{ title: "Login to Wild Knight" }}
-          >
-            {(props) => <Login {...props} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      )}
+          {(props) => <SignUp {...props} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPassword}
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTransparent: true,
+            headerTitle: "",
+            headerLeft: () => (
+              <HeaderBackButton
+                onPress={() => navigation.goBack()}
+                labelVisible={false}
+                testID="forgot-password-back-button"
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name="Map">
+          {(props) => <MapOverview {...props} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name="OrderMenu"
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTransparent: true,
+            headerTitle: "",
+            headerLeft: () => (
+              <HeaderBackButton
+                onPress={() => navigation.goBack()}
+                backImage={() => (
+                  <Icon name="arrow-back" size={24} color="black" />
+                )}
+                labelVisible={false}
+                testID="back-button"
+              />
+            ),
+          })}
+        >
+          {(props) => <OrderMenu {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="OrderPlaced">
+          {(props) => <OrderPlaced {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
