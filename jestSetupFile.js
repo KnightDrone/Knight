5c498d446f7149af6f158307f203c66b9e2412d1
EduardoNeville <eduardoneville82@gmail.com>
2024-04-26 15:39:00 +0200

    imported new firebase library

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -35,7 +35,6 @@ jest.mock("firebase/auth", () => ({
   createUserWithEmailAndPassword: jest.fn(() =>
     Promise.resolve({ user: true })
   ), // Explicitly return a resolved promise
-  signInWithPopup: jest.fn(() => Promise.resolve({ user: true })), // Explicitly return a resolved promise
   signInWithRedirect: jest.fn(() => Promise.resolve({ user: true })), // Explicitly return a resolved promise
   sendPasswordResetEmail: jest.fn(() => Promise.resolve()),
   onAuthStateChanged: jest.fn(),
