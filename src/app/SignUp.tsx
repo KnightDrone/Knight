1d7cc4f13876712fe590517bc101f781f59023ef
Zak <zakaria.laribi@epfl.ch>
2024-04-28 19:18:23 +0200

    removed more logs

--- src/app/SignUp.tsx
+++ src/app/SignUp.tsx
@@ -39,7 +39,6 @@ export default function SignUp({ navigation }: any) {
       const credential = GoogleAuthProvider.credential(id_token);
       signInWithCredential(auth, credential)
         .then(() => {
-          console.log("Sign up successful");
           navigation.navigate("Map");
         })
         .catch((error) => {
@@ -97,7 +96,6 @@ export default function SignUp({ navigation }: any) {
     if (email && password) {
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-          console.log(email, "sign up with email successful.");
           navigation.navigate("Map");
         })
         .catch((error) => {
