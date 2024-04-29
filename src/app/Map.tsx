5f2d2c3dcbd518176ad4964906753a698d8b0fac
Alexandros Pantalos <alexispantalos2003@gmail.com>
2024-04-29 18:54:55 +0200

    change the location marker, updtae location in real time

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -1,15 +1,167 @@
-// Map.tsx
-import React, { useState, useEffect } from "react";
+// // Map.tsx
+// import React, { useState, useEffect } from "react";
+// import { StyleSheet, View, TouchableOpacity, Text, Alert } from "react-native";
+// import MapView, { Marker } from "react-native-maps";
+// import * as Location from "expo-location";
+// import Icon from "react-native-vector-icons/MaterialIcons";
+// import LocationMarker from "../components/LocationMarker";
+
+// const topButtonPadding = 60;
+// const sideButtonPadding = 30;
+
+// // Use the navigation prop to navigate to another screen
+// const MapOverview = ({ navigation }: any) => {
+//   const [currentRegion, setCurrentRegion] = useState({
+//     latitude: 37.789,
+//     longitude: -122.4324,
+//     latitudeDelta: 0.0922,
+//     longitudeDelta: 0.0421,
+//   });
+
+//   const [marker, setMarker] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   const [locationPermission, setLocationPermission] = useState(false);
+
+//   const checkPermissions = async () => {
+//     let { status } = await Location.requestForegroundPermissionsAsync();
+//     if (status !== "granted") {
+//       setLocationPermission(false);
+//       Alert.alert("Permission to access location was denied");
+//       return false;
+//     }
+//     setLocationPermission(true);
+
+//     return true;
+//   };
+
+//   useEffect(() => {
+//     const initMap = async () => {
+//       const allowed = await checkPermissions();
+//       if (allowed) {
+//         await getCurrentLocation();
+//       }
+//     };
+//     initMap();
+//   }, []);
+
+//   const getCurrentLocation = async () => {
+//     const allowed = await checkPermissions();
+//     if (allowed) {
+//       setLoading(true);
+//       let location = await Location.getCurrentPositionAsync({});
+//       const newRegion = {
+//         latitude: location.coords.latitude,
+//         longitude: location.coords.longitude,
+//         latitudeDelta: 0.005,
+//         longitudeDelta: 0.005,
+//       };
+//       setCurrentRegion(newRegion);
+//       setMarker({
+//         latitude: location.coords.latitude,
+//         longitude: location.coords.longitude,
+//       });
+//       setLoading(false);  // Set loading to false after fetching location
+//     } else {
+//       Alert.alert("Location permission not granted");
+//       setLoading(false);  // Ensure loading is set to false even if permission is denied
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container} testID="map-overview-screen">
+//       <MapView
+//         testID="map-view"
+//         style={styles.map}
+//         region={currentRegion}
+//         onRegionChangeComplete={(region) => setCurrentRegion(region)}
+//       >
+//         {marker && (
+//           <LocationMarker
+//           coordinate={marker}
+//           />
+//         )}
+//       </MapView>
+
+//       {loading && (
+//         <View style={styles.loadingContainer}>
+//           <Text>Loading your location...</Text>
+//         </View>
+//       )}
+
+//       <TouchableOpacity
+//         testID="get-location-button"
+//         style={[styles.button, styles.buttonTopRight]}
+//         onPress={getCurrentLocation}
+//       >
+//         <Icon name="my-location" size={24} color="#000" />
+//       </TouchableOpacity>
+
+//       <TouchableOpacity
+//         testID="order-button"
+//         style={[styles.button, styles.buttonBottomRight]}
+//         onPress={() => navigation.navigate("OrderMenu")}
+//       >
+//         <Text style={styles.buttonText}>Order</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+//   button: {
+//     position: "absolute",
+//     backgroundColor: "rgba(255,255,255, 1)",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     borderRadius: 20,
+//     padding: 15,
+//   },
+//   buttonTopLeft: {
+//     top: topButtonPadding,
+//     left: sideButtonPadding,
+//   },
+//   buttonTopRight: {
+//     top: topButtonPadding,
+//     right: sideButtonPadding,
+//   },
+//   buttonBottomRight: {
+//     bottom: 40,
+//     right: sideButtonPadding,
+//   },
+//   buttonText: {
+//     fontSize: 20,
+//     color: "#000",
+//   },
+//   loadingContainer: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "rgba(255,255,255, 0.7)",
+//   },
+// });
+
+// export default MapOverview;
+
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, View, TouchableOpacity, Text, Alert } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import LocationMarker from "../components/LocationMarker";
 
 const topButtonPadding = 60;
 const sideButtonPadding = 30;
 
-// Use the navigation prop to navigate to another screen
 const MapOverview = ({ navigation }: any) => {
+  const mapRef = useRef(null);
   const [currentRegion, setCurrentRegion] = useState({
     latitude: 37.789,
     longitude: -122.4324,
@@ -17,75 +169,96 @@ const MapOverview = ({ navigation }: any) => {
     longitudeDelta: 0.0421,
   });
 
-  const [marker, setMarker] = useState({
-    latitude: 37.789,
-    longitude: -122.4324,
-  });
+  const [marker, setMarker] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const [locationPermission, setLocationPermission] = useState(false);
+  useEffect(() => {
+    const initMap = async () => {
+      const allowed = await checkPermissions();
+      if (allowed) {
+        watchLocation();
+      }
+    };
+    initMap();
+
+    return () => {
+      if (locationWatcher) {
+        locationWatcher.remove();
+      }
+    };
+  }, []);
 
   const checkPermissions = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
-      setLocationPermission(false);
       Alert.alert("Permission to access location was denied");
       return false;
     }
-    setLocationPermission(true);
-
     return true;
   };
 
-  useEffect(() => {
-    checkPermissions();
-  }, []);
+  let locationWatcher: { remove: any } | null = null;
 
-  const getCurrentLocation = async () => {
-    const allowed = await checkPermissions();
-    if (allowed) {
-      let location = await Location.getCurrentPositionAsync({});
+  const watchLocation = async () => {
+    setLoading(true);
+    locationWatcher = await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.High,
+        distanceInterval: 0.1,
+      },
+      (location) => {
+        const newRegion = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        };
+        setCurrentRegion(newRegion);
+        setMarker(newRegion);
+        setLoading(false);
+      }
+    );
+  };
+
+  const centerMapOnCurrentLocation = () => {
+    if (marker) {
       const newRegion = {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
+        latitude: marker.latitude,
+        longitude: marker.longitude,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       };
-      setCurrentRegion(newRegion);
-      setMarker({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
+      mapRef.current.animateToRegion(newRegion, 1000); // Animate map to new region
     } else {
-      Alert.alert("Location permission not granted");
+      Alert.alert("Current location not available");
     }
   };
 
   return (
-    <View style={styles.container} testID="map-overview-screen">
+    <View style={styles.container}>
       <MapView
-        testID="map-view"
+        ref={mapRef}
         style={styles.map}
         region={currentRegion}
-        onRegionChangeComplete={(region) => setCurrentRegion(region)}
+        onRegionChangeComplete={setCurrentRegion}
       >
-        <Marker
-          testID="map-marker"
-          coordinate={marker}
-          title="Current Location"
-        />
+        {marker && <LocationMarker coordinate={marker} />}
       </MapView>
 
+      {loading && (
+        <View style={styles.loadingContainer}>
+          <Text>Loading your location...</Text>
+        </View>
+      )}
+
       <TouchableOpacity
-        testID="get-location-button"
         style={[styles.button, styles.buttonTopRight]}
-        onPress={getCurrentLocation}
+        onPress={centerMapOnCurrentLocation}
       >
-        {/* top right button */}
         <Icon name="my-location" size={24} color="#000" />
       </TouchableOpacity>
 
       <TouchableOpacity
-        testID="order-button"
         style={[styles.button, styles.buttonBottomRight]}
         onPress={() => navigation.navigate("OrderMenu")}
       >
@@ -110,10 +283,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 15,
   },
-  buttonTopLeft: {
-    top: topButtonPadding,
-    left: sideButtonPadding,
-  },
   buttonTopRight: {
     top: topButtonPadding,
     right: sideButtonPadding,
@@ -125,6 +294,12 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: "#000",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255,255,255, 0.7)",
   },
 });
 
---
+++ src/components/LocationMarker.tsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Animated } from "react-native";
+import * as Location from "expo-location";
+import { Marker } from "react-native-maps";
+
+interface LocationMarkerProps {
+  coordinate: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+const LocationMarker: React.FC<LocationMarkerProps> = ({ coordinate }) => {
+  const [heading, setHeading] = useState(0);
+  const fadeAnim = useState(new Animated.Value(0))[0];
+  const scaleAnim = useState(new Animated.Value(0))[0];
+
+  useEffect(() => {
+    let subscription: Location.LocationSubscription | null = null;
+
+    const subscribeHeading = async () => {
+      subscription = await Location.watchHeadingAsync((headingData) => {
+        setHeading(headingData.magHeading);
+      });
+    };
+
+    subscribeHeading();
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const animate = () => {
+      Animated.loop(
+        Animated.sequence([
+          Animated.parallel([
+            Animated.timing(fadeAnim, {
+              toValue: 1,
+              duration: 1000,
+              useNativeDriver: true,
+            }),
+            Animated.timing(scaleAnim, {
+              toValue: 1,
+              duration: 500,
+              useNativeDriver: true,
+            }),
+          ]),
+          Animated.parallel([
+            Animated.timing(fadeAnim, {
+              toValue: 0,
+              duration: 500,
+              useNativeDriver: true,
+            }),
+            Animated.timing(scaleAnim, {
+              toValue: 1,
+              duration: 500,
+              useNativeDriver: true,
+            }),
+            Animated.timing(fadeAnim, {
+              toValue: 0,
+              duration: 1000,
+              useNativeDriver: true,
+            }),
+          ]),
+        ])
+      ).start();
+    };
+
+    animate();
+  }, []);
+
+  return (
+    <Marker coordinate={coordinate}>
+      <View style={styles.container}>
+        <View style={styles.innerCircle} />
+        <Animated.View
+          style={[
+            styles.directionalLight,
+            {
+              opacity: fadeAnim,
+              transform: [{ scale: scaleAnim }],
+            },
+          ]}
+        />
+        <Animated.View
+          style={[
+            styles.arrow,
+            {
+              transform: [{ rotate: `${heading}deg` }, { translateY: -16 }], // Offset to make the triangle's base less visible
+            },
+          ]}
+        />
+      </View>
+    </Marker>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+  },
+  directionalLight: {
+    position: "absolute",
+    width: 35,
+    height: 35,
+    backgroundColor: "rgba(0,122,255,0.3)",
+    borderRadius: 20,
+  },
+  innerCircle: {
+    width: 15,
+    height: 15,
+    borderRadius: 10,
+    backgroundColor: "rgb(0, 122, 255)",
+  },
+  arrow: {
+    position: "absolute",
+    width: 0,
+    height: 0,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderBottomWidth: 8,
+    borderStyle: "solid",
+    backgroundColor: "transparent",
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "rgb(0, 122, 255)",
+  },
+});
+
+export default LocationMarker;
