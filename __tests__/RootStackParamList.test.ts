509a242399ee7cd3ec4ca0b876ceadb1b42ad9f3
Zak <zakaria.laribi@epfl.ch>
2024-04-30 16:41:07 +0200

    rootstackparamlist test

--- __tests__/RootStackParamList.test.ts
+++ __tests__/RootStackParamList.test.ts
@@ -1,17 +1,17 @@
 import { Item } from "../src/types/Item";
 import { RootStackParamList } from "../src/types/RootStackParamList";
 
-const orderedItem: Item = new Item(1, "item 1", "description", 0, 0, 1.99);
 const mockRootStackParamList: RootStackParamList = {
   Login: undefined,
   SignUp: undefined,
   ForgotPassword: undefined,
-  OrderMenu: undefined,
+  OrderMenu: {
+    latitude: -999,
+    longitude: -999,
+  },
   Map: undefined,
   OrderPlaced: {
-    orderedItem: orderedItem,
-    placedAt: Date.now(),
-    userLocation: "123 Main St",
+    orderId: "1",
   },
   OrderHistory: {
     userId: "123",
@@ -37,18 +37,17 @@ describe("RootStackParamList", () => {
   });
 
   it("should have the correct types", () => {
-    expect(mockRootStackParamList.OrderPlaced).toHaveProperty("orderedItem");
-    expect(mockRootStackParamList.OrderPlaced).toHaveProperty("placedAt");
-    expect(mockRootStackParamList.OrderPlaced).toHaveProperty("userLocation");
+    expect(mockRootStackParamList.OrderMenu).toHaveProperty("latitude");
+    expect(mockRootStackParamList.OrderMenu).toHaveProperty("longitude");
+    expect(mockRootStackParamList.OrderPlaced).toHaveProperty("orderId");
     expect(mockRootStackParamList.OrderHistory).toHaveProperty("opOrders");
     expect(mockRootStackParamList.OrderHistory).toHaveProperty("userId");
   });
 
   it("should have the correct values", () => {
-    expect(mockRootStackParamList.OrderPlaced.orderedItem).toBeInstanceOf(Item);
-    expect(mockRootStackParamList.OrderPlaced.userLocation).toEqual(
-      "123 Main St"
-    );
+    expect(mockRootStackParamList.OrderPlaced.orderId).toEqual("1");
     expect(mockRootStackParamList.OrderHistory.opOrders).toBeTruthy();
+    expect(mockRootStackParamList.OrderMenu.latitude).toEqual(-999);
+    expect(mockRootStackParamList.OrderMenu.longitude).toEqual(-999);
   });
 });
