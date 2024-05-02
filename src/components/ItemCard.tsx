8cdb63820513a3bc3ae2b8c8b9a10b787181e768
kalil0321 <kalil.bouzigues@gmail.com>
2024-05-02 15:54:32 +0200

    fix items i18n

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -4,6 +4,7 @@ import { Item } from "../types/Item";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { BlurView } from "expo-blur";
 import { PayButton } from "./PayButton";
+import { useTranslation } from "react-i18next";
 
 interface ItemCardProps {
   isVisible: boolean;
@@ -22,6 +23,8 @@ function ItemCard({
     return null;
   }
 
+  const { t } = useTranslation();
+
   return (
     <View style={styles.visibleCard} testID={`item-card-view-${item.getId()}`}>
       <BlurView intensity={10} style={styles.blurContainer} testID="blur-view">
@@ -33,8 +36,8 @@ function ItemCard({
           >
             <Icon name="close" size={20} color="#000" testID="close-icon" />
           </TouchableOpacity>
-          <Text style={styles.title}>{item.getName()}</Text>
-          <Text style={styles.description}>{item.getDescription()}</Text>
+          <Text style={styles.title}>{t(item.getName())}</Text>
+          <Text style={styles.description}>{t(item.getDescription())}</Text>
           <Image
             style={styles.image}
             source={item.getImage()}
