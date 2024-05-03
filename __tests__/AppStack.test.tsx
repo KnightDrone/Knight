80411c8c60c8ce5f05cf7dd1f575a7e318d76254
Alexandros Pantalos <alexispantalos2003@gmail.com>
2024-05-03 10:48:45 +0200

    test for AppStack

--- __tests__/AppStack.test.tsx
+++ __tests__/AppStack.test.tsx
@@ -1,277 +1,177 @@
-// import * as React from "react";
-// import {
-//   screen,
-//   render,
-//   fireEvent,
-//   waitFor,
-// } from "@testing-library/react-native";
-// import { AppStack } from "../src/navigation/AppStack";
-// import { describe } from "node:test";
-// import * as Google from "expo-auth-session/providers/google";
-
-// import { signInWithEmailAndPassword } from "../src/services/Firebase";
-
-// beforeEach(() => {
-//   const mockPromptAsync = jest.fn();
-
-//   // Mock the Google authentication request setup, makes the user sign in automatically with google
-//   (Google.useAuthRequest as jest.Mock).mockReturnValue([
-//     {}, // Mocked request
-//     { type: "success", params: { id_token: "mock-id-token" } }, // Mocked response
-//     mockPromptAsync, // Mocked promptAsync function
-//   ]);
-// });
-
-// beforeAll(() => {
-//   global.alert = jest.fn();
-
-//   // Avoid useless error messages
-//   jest.spyOn(console, "error").mockImplementation(() => {});
-// });
-
-// jest.mock("../src/services/Firebase", () => ({
-//   auth: jest.fn(),
-//   GoogleAuthProvider: {
-//     credential: jest.fn(),
-//   },
-//   signInWithCredential: jest.fn(() =>
-//     Promise.resolve({ user: { uid: "12345" } })
-//   ),
-//   signInWithEmailAndPassword: jest.fn(() =>
-//     Promise.resolve({ user: { uid: "12345" } })
-//   ),
-// }));
-
-// jest.mock("expo-auth-session/providers/google", () => ({
-//   useAuthRequest: jest.fn(() => [
-//     {}, // request
-//     { type: "success", params: { id_token: "fake_token" } }, // response
-//     jest.fn(), // promptAsync, simulating successful prompt
-//   ]),
-// }));
-
-// jest.mock("react-native-vector-icons/MaterialIcons", () => {
-//   const { View } = require("react-native");
-//   return {
-//     __esModule: true,
-//     default: jest.fn().mockImplementation((props) => <View {...props} />),
-//   };
-// });
-
-// //// =*=*=*=*=*=*=*=*=*
-// //// Test for AppStack
-// //// =*=*=*=*=*=*=*=*=*
-
-// describe("AppStack Navigation Tests", () => {
-//   it("navigates to the Login screen when app starts", () => {
-//     const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-//     // Check if the Login screen is displayed by looking for a specific text
-//     expect(getByTestId("login-screen")).toBeTruthy();
-//   });
-
-//   it("navigates to the Map screen when user is logged in", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-//     await waitFor(() => {
-//       // Check if the Map screen is displayed by looking for a specific text
-//       expect(queryByTestId("map-overview-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to ForgotPassword screen when 'Reset Password' is pressed", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-//     await waitFor(() => {
-//       fireEvent.press(queryByTestId("forgot-password-link"));
-//       // Check if the ForgotPassword screen is displayed by looking for a specific text
-//       expect(screen.getByTestId("forgot-password-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to ForgotPassword screen when 'Reset Password' is pressed then navigates back when back-button is pressed", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-
-//     fireEvent.press(queryByTestId("forgot-password-link"));
-//     fireEvent.press(queryByTestId("forgot-password-back-button"));
-//     await waitFor(() => {
-//       // Check if the ForgotPassword screen is displayed by looking for a specific text
-//       expect(screen.getByTestId("login-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to SignUp screen when 'Sign Up' is pressed", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-//     await waitFor(() => {
-//       fireEvent.press(queryByTestId("sign-up-link"));
-//       // Check if the SignUp screen is displayed by looking for a specific text
-//       expect(screen.getByTestId("sign-up-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to SignUp screen when 'Sign Up' is pressed then navigates back when back-button is pressed", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-//     fireEvent.press(queryByTestId("sign-up-link"));
-//     fireEvent.press(queryByTestId("sign-up-back-button"));
-//     await waitFor(() => {
-//       // Check if the SignUp screen is displayed by looking for a specific text
-//       expect(screen.getByTestId("login-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to the map screen after successful sign-up", async () => {
-//     const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-
-//     // Mock is true (successful sign-up) by default in jestSetupFile.js
-//     // Simulate successful sign-up
-//     fireEvent.press(getByTestId("sign-up-link"));
-//     fireEvent.press(getByTestId("sign-up-button"));
-//     // Wait for any async actions to complete
-//     await waitFor(() => {
-//       expect(screen.getByTestId("map-overview-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates Login -> Map -> OrderMenu", async () => {
-//     const { queryByTestId, getByTestId } = render(
-//       <AppStack isLoggedIn={"Login"} />
-//     );
-
-//     fireEvent.changeText(getByTestId("email-input"), "test@example.com");
-//     fireEvent.changeText(getByTestId("password-input"), "password123");
-
-//     fireEvent.press(getByTestId("login-button"));
-
-//     await waitFor(() => expect(signInWithEmailAndPassword).toHaveBeenCalled());
-//     fireEvent.press(getByTestId("order-button"));
-
-//     await waitFor(() =>
-//       expect(queryByTestId("order-menu-screen")).toBeTruthy()
-//     );
-//   });
-
-//   it("navigates Map->OrderMenu", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-//     fireEvent.press(queryByTestId("order-button"));
-//     await waitFor(() => {
-//       // Check if the Map screen is displayed by looking for a specific text
-//       expect(screen.queryByTestId("order-menu-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates to OrderMenu then back to Map screen", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-//     fireEvent.press(queryByTestId("order-button"));
-//     fireEvent.press(queryByTestId("order-menu-back-button"));
-//     await waitFor(() => {
-//       // Check if the OrderMenu screen is displayed by looking for a specific text
-//       expect(screen.getByTestId("map-overview-screen")).toBeTruthy();
-//     });
-//   });
-
-//   it("navigates Map->OrderMenu->OrderPlaced", async () => {
-//     const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-//     fireEvent.press(queryByTestId("order-button"));
-//     await waitFor(() => {
-//       // Check if the Map screen is displayed by looking for a specific text
-//       expect(screen.queryByTestId("order-menu-screen")).toBeTruthy();
-//     });
-//   });
-// });
-
-import React from "react";
-import { render, waitFor, fireEvent } from "@testing-library/react-native";
+import * as React from "react";
+import {
+  screen,
+  render,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react-native";
 import { AppStack } from "../src/navigation/AppStack";
-import { View, Text } from "react-native";
+import { describe } from "node:test";
+import * as Google from "expo-auth-session/providers/google";
 
-jest.mock("react-native-vector-icons/MaterialIcons", () => ({
-  __esModule: true,
-  default: ({ name, size, color, ...props }: any) => (
-    <View {...props}>
-      <Text>
-        {name} - {size} - {color}
-      </Text>
-    </View>
+import { signInWithEmailAndPassword } from "../src/services/Firebase";
+
+beforeEach(() => {
+  const mockPromptAsync = jest.fn();
+
+  // Mock the Google authentication request setup, makes the user sign in automatically with google
+  (Google.useAuthRequest as jest.Mock).mockReturnValue([
+    {}, // Mocked request
+    { type: "success", params: { id_token: "mock-id-token" } }, // Mocked response
+    mockPromptAsync, // Mocked promptAsync function
+  ]);
+});
+
+beforeAll(() => {
+  global.alert = jest.fn();
+
+  // Avoid useless error messages
+  jest.spyOn(console, "error").mockImplementation(() => {});
+});
+
+jest.mock("../src/services/Firebase", () => ({
+  auth: jest.fn(),
+  GoogleAuthProvider: {
+    credential: jest.fn(),
+  },
+  signInWithCredential: jest.fn(() =>
+    Promise.resolve({ user: { uid: "12345" } })
+  ),
+  signInWithEmailAndPassword: jest.fn(() =>
+    Promise.resolve({ user: { uid: "12345" } })
   ),
 }));
+
+jest.mock("expo-auth-session/providers/google", () => ({
+  useAuthRequest: jest.fn(() => [
+    {}, // request
+    { type: "success", params: { id_token: "fake_token" } }, // response
+    jest.fn(), // promptAsync, simulating successful prompt
+  ]),
+}));
+
+jest.mock("react-native-vector-icons/MaterialIcons", () => {
+  const { View } = require("react-native");
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation((props) => <View {...props} />),
+  };
+});
+
+//// =*=*=*=*=*=*=*=*=*
+//// Test for AppStack
+//// =*=*=*=*=*=*=*=*=*
 
 describe("AppStack Navigation Tests", () => {
   it("navigates to the Login screen when app starts", () => {
     const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
+    // Check if the Login screen is displayed by looking for a specific text
     expect(getByTestId("login-screen")).toBeTruthy();
   });
 
   it("navigates to the Map screen when user is logged in", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Map"} />);
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
     await waitFor(() => {
-      expect(getByTestId("map-overview-screen")).toBeTruthy();
+      // Check if the Map screen is displayed by looking for a specific text
+      expect(queryByTestId("map-overview-screen")).toBeTruthy();
     });
   });
 
   it("navigates to ForgotPassword screen when 'Reset Password' is pressed", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-    fireEvent.press(getByTestId("forgot-password-link"));
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
     await waitFor(() => {
-      expect(getByTestId("forgot-password-screen")).toBeTruthy();
+      fireEvent.press(queryByTestId("forgot-password-link"));
+      // Check if the ForgotPassword screen is displayed by looking for a specific text
+      expect(screen.getByTestId("forgot-password-screen")).toBeTruthy();
+    });
+  });
+
+  it("navigates to ForgotPassword screen when 'Reset Password' is pressed then navigates back when back-button is pressed", async () => {
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
+
+    fireEvent.press(queryByTestId("forgot-password-link"));
+    fireEvent.press(queryByTestId("forgot-password-back-button"));
+    await waitFor(() => {
+      // Check if the ForgotPassword screen is displayed by looking for a specific text
+      expect(screen.getByTestId("login-screen")).toBeTruthy();
     });
   });
 
   it("navigates to SignUp screen when 'Sign Up' is pressed", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
-    fireEvent.press(getByTestId("sign-up-link"));
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
     await waitFor(() => {
-      expect(getByTestId("sign-up-screen")).toBeTruthy();
+      fireEvent.press(queryByTestId("sign-up-link"));
+      // Check if the SignUp screen is displayed by looking for a specific text
+      expect(screen.getByTestId("sign-up-screen")).toBeTruthy();
+    });
+  });
+
+  it("navigates to SignUp screen when 'Sign Up' is pressed then navigates back when back-button is pressed", async () => {
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Login"} />);
+    fireEvent.press(queryByTestId("sign-up-link"));
+    fireEvent.press(queryByTestId("sign-up-back-button"));
+    await waitFor(() => {
+      // Check if the SignUp screen is displayed by looking for a specific text
+      expect(screen.getByTestId("login-screen")).toBeTruthy();
     });
   });
 
   it("navigates to the map screen after successful sign-up", async () => {
     const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
+
+    // Mock is true (successful sign-up) by default in jestSetupFile.js
+    // Simulate successful sign-up
     fireEvent.press(getByTestId("sign-up-link"));
     fireEvent.press(getByTestId("sign-up-button"));
+    // Wait for any async actions to complete
     await waitFor(() => {
-      expect(getByTestId("map-overview-screen")).toBeTruthy();
+      expect(screen.getByTestId("map-overview-screen")).toBeTruthy();
     });
   });
 
   it("navigates Login -> Map -> OrderMenu", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Login"} />);
+    const { queryByTestId, getByTestId } = render(
+      <AppStack isLoggedIn={"Login"} />
+    );
+
     fireEvent.changeText(getByTestId("email-input"), "test@example.com");
     fireEvent.changeText(getByTestId("password-input"), "password123");
+
     fireEvent.press(getByTestId("login-button"));
 
-    await waitFor(() => {
-      expect(getByTestId("map-overview-screen")).toBeTruthy();
-    });
-
+    await waitFor(() => expect(signInWithEmailAndPassword).toHaveBeenCalled());
     fireEvent.press(getByTestId("order-button"));
-    await waitFor(() => {
-      expect(getByTestId("order-menu-screen")).toBeTruthy();
-    });
+
+    await waitFor(() =>
+      expect(queryByTestId("order-menu-screen")).toBeTruthy()
+    );
   });
 
-  it("navigates Map -> OrderMenu", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-    fireEvent.press(getByTestId("order-button"));
+  it("navigates Map->OrderMenu", async () => {
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
+    fireEvent.press(queryByTestId("order-button"));
     await waitFor(() => {
-      expect(getByTestId("order-menu-screen")).toBeTruthy();
+      // Check if the Map screen is displayed by looking for a specific text
+      expect(screen.queryByTestId("order-menu-screen")).toBeTruthy();
     });
   });
 
   it("navigates to OrderMenu then back to Map screen", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-    fireEvent.press(getByTestId("order-button"));
-    fireEvent.press(getByTestId("order-menu-back-button"));
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
+    fireEvent.press(queryByTestId("order-button"));
+    fireEvent.press(queryByTestId("order-menu-back-button"));
     await waitFor(() => {
-      expect(getByTestId("map-overview-screen")).toBeTruthy();
+      // Check if the OrderMenu screen is displayed by looking for a specific text
+      expect(screen.getByTestId("map-overview-screen")).toBeTruthy();
     });
   });
 
-  it("navigates Map -> OrderMenu -> OrderPlaced", async () => {
-    const { getByTestId } = render(<AppStack isLoggedIn={"Map"} />);
-    fireEvent.press(getByTestId("order-button"));
+  it("navigates Map->OrderMenu->OrderPlaced", async () => {
+    const { queryByTestId } = render(<AppStack isLoggedIn={"Map"} />);
+    fireEvent.press(queryByTestId("order-button"));
     await waitFor(() => {
-      expect(getByTestId("order-menu-screen")).toBeTruthy();
-    });
-    fireEvent.press(getByTestId("place-order-button"));
-    await waitFor(() => {
-      expect(getByTestId("order-placed-screen")).toBeTruthy();
+      // Check if the Map screen is displayed by looking for a specific text
+      expect(screen.queryByTestId("order-menu-screen")).toBeTruthy();
     });
   });
 });
