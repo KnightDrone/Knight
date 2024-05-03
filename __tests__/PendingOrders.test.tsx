148dc5cb91dbccc0af69091e8e1c508cd4039bc9
kalil0321 <kalil.bouzigues@gmail.com>
2024-05-03 08:23:04 +0200

    update PendingOrders test

--- __tests__/PendingOrders.test.tsx
+++ __tests__/PendingOrders.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { act, render } from "@testing-library/react-native";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import PendingOrders from "../src/app/PendingOrders";
 import { Order, OrderStatus } from "../src/types/Order";
 import { Item } from "../src/types/Item";
-import queryOrder from "../src/services/FirestoreManager";
+import FirestoreManager from "../src/services/FirestoreManager";
 
 jest.mock("../src/services/FirestoreManager", () => {
   return {
@@ -23,7 +23,8 @@ jest.mock("../src/services/FirestoreManager", () => {
                 new Date(),
                 "Mattenhorn peak #3",
                 "St. Gallen Hospital",
-                { latitude: 55, longitude: 33 }
+                { latitude: 55, longitude: 33 },
+                "1"
               ),
               new Order(
                 "user2",
@@ -34,7 +35,8 @@ jest.mock("../src/services/FirestoreManager", () => {
                 new Date(),
                 "Mattenhorn peak #1",
                 "Pharmacy #5",
-                { latitude: 55, longitude: 33 }
+                { latitude: 55, longitude: 33 },
+                "2"
               ),
             ])
           ),
@@ -67,18 +69,79 @@ describe("PendingOrders Component", () => {
     expect(orderList).toBeDefined();
   });
 
-  it.only("fetches orders", () => {
-    jest.useFakeTimers();
+  it("fetches orders", async () => {
     const { getByTestId } = render(<PendingOrders />);
-
-    act(() => {
-      jest.advanceTimersByTime(10_000); // 10s
-    });
 
     const orderList = getByTestId("order-list");
     expect(orderList).toBeDefined();
 
-    const orderItems = orderList.children;
-    expect(orderItems).toHaveLength(2);
+    await waitFor(() => {
+      expect(getByTestId("order-card-1")).toBeDefined();
+      expect(getByTestId("order-card-2")).toBeDefined();
+    });
+
+    await waitFor(() => {
+      const button = getByTestId("order-card-1-button");
+      expect(button).toBeDefined();
+      fireEvent.press(button);
+    });
+
+    await waitFor(() => {
+      const closeButton = getByTestId("close-card-button");
+      expect(closeButton).toBeDefined();
+      fireEvent.press(closeButton);
+    });
+  });
+
+  it("fails to fetch orders", async () => {
+    (FirestoreManager as jest.Mock).mockImplementationOnce(() => ({
+      queryOrder: jest.fn().mockReturnValue(null),
+    }));
+
+    const { getByText, getByTestId } = render(<PendingOrders />);
+
+    await waitFor(
+      () => {
+        expect(getByTestId("error-box")).toBeTruthy();
+        expect(getByText("Failed to fetch from database.")).toBeTruthy();
+      },
+      { timeout: 2000 }
+    );
+
+    fireEvent.press(getByTestId("error-box"));
+  });
+
+  it("returns empty orders", async () => {
+    (FirestoreManager as jest.Mock).mockImplementationOnce(() => ({
+      queryOrder: jest.fn().mockReturnValue([]),
+    }));
+
+    const { getByText, getByTestId } = render(<PendingOrders />);
+
+    await waitFor(
+      () => {
+        expect(getByTestId("error-box")).toBeTruthy();
+        expect(
+          getByText("No orders pending orders at the moment, check back later.")
+        ).toBeTruthy();
+      },
+      { timeout: 2000 }
+    );
+  });
+
+  it("queryOrder throws an error", async () => {
+    (FirestoreManager as jest.Mock).mockImplementationOnce(() => ({
+      queryOrder: jest.fn().mockRejectedValue(new Error("Query failed.")),
+    }));
+
+    const { getByText, getByTestId } = render(<PendingOrders />);
+
+    await waitFor(
+      () => {
+        expect(getByTestId("error-box")).toBeTruthy();
+        expect(getByText("Query failed.")).toBeTruthy();
+      },
+      { timeout: 2000 }
+    );
   });
 });
