f708d447f696fe32007bfa33d482fab5a4562c42
Zak <zakaria.laribi@epfl.ch>
2024-05-08 12:49:12 +0200

    fixed directories for icons

--- src/app/order/PendingOrders.tsx
+++ src/app/order/PendingOrders.tsx
@@ -15,42 +15,6 @@ import TriangleBackground from "../../components/TriangleBackground";
 import FirestoreManager from "../../services/FirestoreManager";
 import { MessageBox } from "../../ui/MessageBox";
 
-/* 
-NOTE: This is a temporary solution to simulate fetching pending orders from a server. Should be replaced with actual database calls
-*/
-/*const fetchPendingOrdersMock = async (): Promise<Order[]> => {
-  return new Promise((resolve) => {
-    setTimeout(async () => {
-      const orders: Order[] = [
-        // Replace with predefined set pending orders
-        new Order(
-          "user1",
-          new Item(1, "item1", "description1", 1, 1, 10),
-          { latitude: 46.8182, longitude: 8.2275 } // Correct way to create an OrderLocation object
-        ),
-        new Order(
-          "user2",
-          new Item(2, "item2", "description2", 2, 2, 22),
-          { latitude: 40.8182, longitude: 8.2275 } // Correct way to create an OrderLocation object
-        ),
-        new Order(
-          "user3",
-          new Item(3, "item3", "description3", 3, 3, 330),
-          { latitude: 0, longitude: 0 } // Correct way to create an OrderLocation object
-        ),
-        new Order(
-          "user4",
-          new Item(3, "item4", "description3", 3, 3, 330),
-          { latitude: 20, longitude: 50 } // Correct way to create an OrderLocation object
-        ),
-      ];
-      // Call locSearch for each order and wait for all to complete, Nominatim API to search
-      await Promise.all(orders.map((order) => order.locSearch()));
-      resolve(orders);
-    }, 1000); // 1 second delay
-  });
-};*/
-
 // TODO: Maybe add some search bar to filter?
 
 const PendingOrders = ({ navigation }: any) => {
@@ -108,7 +72,7 @@ const PendingOrders = ({ navigation }: any) => {
       <View className="flex-row items-center justify-center">
         <TouchableOpacity className="absolute left-4" testID="menu-button">
           <Image
-            source={require("../../assets/icons/menu_icon.png")}
+            source={require("../../../assets/icons/menu_icon.png")}
             className="w-9 h-9"
             testID="menu-icon"
           />
@@ -125,7 +89,7 @@ const PendingOrders = ({ navigation }: any) => {
           onPress={() => navigation.goBack()}
         >
           <Image
-            source={require("../../assets/icons/x_icon.png")}
+            source={require("../../../assets/icons/x_icon.png")}
             className="w-5 h-5"
             testID="close-icon"
           />
@@ -175,7 +139,7 @@ const PendingOrders = ({ navigation }: any) => {
                 className="absolute right-5 top-5 "
               >
                 <Image
-                  source={require("../../assets/icons/x_icon.png")}
+                  source={require("../../../assets/icons/x_icon.png")}
                   className="w-5 h-5"
                 />
               </TouchableOpacity>
