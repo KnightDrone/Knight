715f728bc28aed880263238671c680fcb6d1d3f7
kalil0321 <kalil.bouzigues@gmail.com>
2024-05-17 04:33:39 +0200

    refactor AppStack component to use separate drawer components for User and Operator navigation

--- src/app/AppStack.tsx
+++ src/app/AppStack.tsx
@@ -3,8 +3,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { HeaderBackButton } from "@react-navigation/elements";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootStackParamList } from "../types/RootStackParamList";
-import { DrawerParamList } from "../types/DrawerParamList";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
 // Stack Navigation Screens
 import { User } from "../services/Firebase";
@@ -12,108 +10,12 @@ import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
 import ForgotPassword from "./auth/ForgotPassword";
 import OrderPlaced from "./order/OrderPlaced";
-import OrderMenu from "./order/OrderMenu";
 import PendingOrders from "./order/PendingOrders";
-import MapOverview from "./Map";
 
-// Drawer Navigation Screens
-import Profile from "./settings/ProfileScreen";
-import Setting from "./settings/Setting";
-import OrderHistory from "./order/OrderHistory";
+import { UserDrawer } from "../components/drawers/UserDrawer";
+import { OperatorDrawer } from "../components/drawers/OperatorDrawer";
 
 const { Navigator, Screen, Group } = createStackNavigator<RootStackParamList>();
-
-const Drawer = createDrawerNavigator<DrawerParamList>();
-
-interface UserDrawerProps {
-  user?: User | null;
-}
-
-function UserDrawer<UserDrawerProps>(user: UserDrawerProps) {
-  return (
-    <Drawer.Navigator initialRouteName="Map">
-      <Drawer.Screen name="Map" options={{ headerShown: false }}>
-        {(props: any) => <MapOverview {...props} />}
-      </Drawer.Screen>
-      <Drawer.Screen
-        name="Profile"
-        options={({ navigation }: any) => ({
-          headerShown: true,
-          headerTransparent: true,
-          headerTitle: "",
-          headerLeft: () => (
-            <HeaderBackButton
-              onPress={() => navigation.toggleDrawer()}
-              labelVisible={false}
-              testID="profile-drawer-button" // Ensure this testID is correctly set
-            />
-          ),
-        })}
-      >
-        {(props: any) => {
-          return <Profile {...props} />;
-        }}
-      </Drawer.Screen>
-      <Drawer.Screen
-        name="Settings"
-        options={({ navigation }: any) => ({
-          headerShown: true,
-          headerTransparent: true,
-          headerTitle: "",
-          headerLeft: () => (
-            <HeaderBackButton
-              onPress={() => navigation.toggleDrawer()}
-              labelVisible={false}
-              testID="settings-back-button"
-            />
-          ),
-        })}
-      >
-        {(props: any) => <Setting {...props} />}
-      </Drawer.Screen>
-      <Drawer.Screen
-        name="OrderMenu"
-        options={({ navigation }: any) => ({
-          headerShown: true,
-          headerTransparent: true,
-          headerTitle: "",
-          headerLeft: () => (
-            <HeaderBackButton
-              onPress={() => navigation.toggleDrawer()}
-              labelVisible={false}
-              testID="order-menu-drawer-back-button"
-            />
-          ),
-        })}
-      >
-        {(props: any) => <OrderMenu {...props} />}
-      </Drawer.Screen>
-
-      <Drawer.Screen
-        name="OrderHistory"
-        options={({ navigation }: any) => ({
-          headerShown: true,
-          headerTransparent: true,
-          headerTitle: "",
-          headerLeft: () => (
-            <HeaderBackButton
-              onPress={() => navigation.toggleDrawer()}
-              labelVisible={false}
-              testID="order-history-back-button"
-            />
-          ),
-        })}
-      >
-        {(props: any) => (
-          <OrderHistory
-            {...props}
-            route={{ params: { historyOp: false, user } }}
-          />
-        )}
-      </Drawer.Screen>
-    </Drawer.Navigator>
-  );
-}
 
 interface AppStackProps {
   isLoggedIn: "Login" | "UserDrawer";
@@ -177,7 +79,9 @@ export const AppStack: React.FC<AppStackProps> = ({ isLoggedIn, user }) => {
           <Screen name="UserDrawer">
             {(props: any) => <UserDrawer {...props} user={{ user }} />}
           </Screen>
-
+          <Screen name="OperatorDrawer">
+            {(props: any) => <OperatorDrawer {...props} />}
+          </Screen>
           <Screen name="OrderPlaced">
             {(props: any) => <OrderPlaced {...props} />}
           </Screen>
