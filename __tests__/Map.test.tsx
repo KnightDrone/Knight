2faedc8bfccadb0f339580773972c6e02c0c2cb5
kalil0321 <kalil.bouzigues@gmail.com>
2024-05-19 16:27:36 +0200

    update test

--- __tests__/Map.test.tsx
+++ __tests__/Map.test.tsx
@@ -14,6 +14,7 @@ jest.mock("expo-location", () => {
 });
 
 jest.mock("react-native-vector-icons/MaterialIcons", () => "Icon");
+jest.mock("react-native-vector-icons/MaterialCommunityIcons", () => "Icon");
 jest.mock("../src/components/LocationMarker", () => "LocationMarker");
 
 describe("MapOverview", () => {
