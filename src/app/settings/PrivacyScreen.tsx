72a42b8793d6c8f22d22c80dce60f6fba3cef562
Zak <zakaria.laribi@epfl.ch>
2024-05-21 07:15:13 +0200

    created privacy screen

--- src/app/settings/PrivacyScreen.tsx
+++ src/app/settings/PrivacyScreen.tsx
@@ -1,27 +1,105 @@
 import React from "react";
-import Button from "../../components/buttons/OrderButton";
+import { View, StyleSheet, TouchableOpacity, Text, Switch } from "react-native";
 
-const PrivacyScreen = () => {
+const PrivacyScreen = (navigation: any) => {
+  const [isAdsEnabled, setIsAdsEnabled] = React.useState(true);
+  const [isTrackingEnabled, setIsTrackingEnabled] = React.useState(true);
+  const [isLocationEnabled, setIsLocationEnabled] = React.useState(true);
+
+  const toggleAds = () => setIsAdsEnabled((previousState) => !previousState);
+  const toggleTracking = () =>
+    setIsTrackingEnabled((previousState) => !previousState);
+  const toggleLocation = () =>
+    setIsLocationEnabled((previousState) => !previousState);
+
   return (
-    <div>
-      <div>
-        <Button
-          title="Data tracking"
-          onPress={() => console.log("Data tracking")}
-          icon={0}
-        />
-        ,
-      </div>
-      <div>
-        <Button
-          title="Terms of Service"
-          onPress={() => console.log("Terms of service")}
-          icon={0}
-        />
-        ,
-      </div>
-    </div>
+    <View style={styles.container}>
+      <View style={styles.boxes}>
+        <TouchableOpacity style={styles.buttons}>
+          <View style={styles.textView}>
+            <Text style={styles.text}>Data Tracking</Text>
+            <Text style={styles.subtext}>
+              Control what data is used for personalization
+            </Text>
+          </View>
+          <Switch
+            trackColor={{ false: "#767577", true: "#A0D1E4" }}
+            onValueChange={toggleTracking}
+            value={isTrackingEnabled}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <View style={styles.textView}>
+            <Text style={styles.text}>Personalized Ads</Text>
+            <Text style={styles.subtext}>
+              For a more tailored ad experience
+            </Text>
+          </View>
+          <Switch
+            trackColor={{ false: "#767577", true: "#A0D1E4" }}
+            onValueChange={toggleAds}
+            value={isAdsEnabled}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <View style={styles.textView}>
+            <Text style={styles.text}>Share Live Location</Text>
+            <Text style={styles.subtext}>
+              Allow access to your location in case of emergency
+            </Text>
+          </View>
+          <Switch
+            trackColor={{ false: "#767577", true: "#A0D1E4" }}
+            onValueChange={toggleLocation}
+            value={isLocationEnabled}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Terms of Service")}
+          style={styles.buttons}
+        >
+          <Text style={styles.text}>Terms of Service</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f7f7f7",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  textView: {
+    flexDirection: "column",
+  },
+  boxes: {
+    marginTop: 100,
+  },
+  buttons: {
+    backgroundColor: "#ffffff",
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 18,
+    color: "#333",
+  },
+  subtext: {
+    fontSize: 12,
+    color: "#666",
+  },
+});
 
 export default PrivacyScreen;
