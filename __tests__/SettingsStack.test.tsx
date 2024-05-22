eeafbdf7209dc4121ca3c24277614019a9fea99a
Zak <zakaria.laribi@epfl.ch>
2024-05-22 16:50:11 +0200

    sorted out settings stack tests

--- __tests__/SettingsStack.test.tsx
+++ __tests__/SettingsStack.test.tsx
@@ -3,6 +3,29 @@ import { render } from "@testing-library/react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { SettingsStack } from "../src/app/settings/SettingsStack";
 
+jest.mock("../src/services/Firebase", () => ({
+  auth: {
+    currentUser: {
+      uid: "testUserId",
+    },
+  },
+}));
+
+jest.mock("../src/services/FirestoreManager.ts", () => {
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation(() => {
+      return {
+        writeData: jest.fn().mockResolvedValue({}),
+        queryData: jest.fn().mockResolvedValue([]),
+        queryOrder: jest.fn().mockResolvedValue([]),
+        getUser: jest.fn().mockResolvedValue({}),
+        createUser: jest.fn().mockResolvedValue({}),
+      };
+    }),
+  };
+});
+
 describe("SettingsStack", () => {
   it("renders correctly", () => {
     const { getByText } = render(
@@ -11,7 +34,6 @@ describe("SettingsStack", () => {
       </NavigationContainer>
     );
 
-    expect(getByText("Settings")).toBeTruthy();
     expect(getByText("Edit profile")).toBeTruthy();
     expect(getByText("FAQs")).toBeTruthy();
     expect(getByText("TermsAndConditions")).toBeTruthy();
