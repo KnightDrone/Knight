ec331b37bd294bc288379163ff46bc0ba4002312
Zak <zakaria.laribi@epfl.ch>
2024-05-22 13:07:14 +0200

    added settings stack tests

---
+++ __tests__/SettingsStack.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { SettingsStack } from "../src/app/settings/SettingsStack";
+
+describe("SettingsStack", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <NavigationContainer>
+        <SettingsStack userId="testUserId" />
+      </NavigationContainer>
+    );
+
+    expect(getByText("Settings")).toBeTruthy();
+    expect(getByText("Profile")).toBeTruthy();
+    expect(getByText("FAQs")).toBeTruthy();
+    expect(getByText("TermsAndConditions")).toBeTruthy();
+    expect(getByText("Privacy")).toBeTruthy();
+    expect(getByText("Notifications")).toBeTruthy();
+  });
+});
