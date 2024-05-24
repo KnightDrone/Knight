7a7e307db4e544597ea3d76cf2839cddf60ce906
Zak <zakaria.laribi@epfl.ch>
2024-05-24 10:59:35 +0200

    added padding to header

--- src/app/settings/SettingsStack.tsx
+++ src/app/settings/SettingsStack.tsx
@@ -6,6 +6,8 @@ import TermsAndConditions from "./TermsAndConditions";
 import PrivacyScreen from "./PrivacyScreen";
 import NotificationsScreen from "./NotificationsScreen";
 import { SettingsStackParamList } from "../../types/SettingsStackParamList";
+import { View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const { Navigator, Screen, Group } =
   createStackNavigator<SettingsStackParamList>();
@@ -18,24 +20,36 @@ interface SettingsStackProps {
 
 export const SettingsStack: React.FC<SettingsStackProps> = ({ userId }) => {
   return (
-    <Stack.Navigator initialRouteName="Settings">
-      <Stack.Screen
-        name="Settings"
-        component={Setting}
-        initialParams={{ userId: userId }}
-        options={{
-          header: () => null,
-        }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        initialParams={{ userId: userId }}
-      />
-      <Stack.Screen name="FAQs" component={FAQs} />
-      <Stack.Screen name="TermsAndConditions" component={TermsAndConditions} />
-      <Stack.Screen name="Privacy" component={PrivacyScreen} />
-      <Stack.Screen name="Notifications" component={NotificationsScreen} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Stack.Navigator initialRouteName="Settings">
+        <Stack.Screen
+          name="Settings"
+          component={Setting}
+          initialParams={{ userId: userId }}
+          options={{
+            header: () => null,
+          }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          initialParams={{ userId: userId }}
+        />
+        <Stack.Screen name="FAQs" component={FAQs} />
+        <Stack.Screen
+          name="TermsAndConditions"
+          component={TermsAndConditions}
+        />
+        <Stack.Screen name="Privacy" component={PrivacyScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      </Stack.Navigator>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+});
