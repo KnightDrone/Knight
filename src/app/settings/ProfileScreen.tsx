f24decce632020cc69f3018c13b6785f823124fd
CHR0M05T0N3D <93383002+CHR0M05T0N3D@users.noreply.github.com>
2024-06-01 12:15:28 +0200

    Updated profile for correct visualization within android

--- src/app/settings/ProfileScreen.tsx
+++ src/app/settings/ProfileScreen.tsx
@@ -16,6 +16,7 @@ import FirestoreManager, { DBUser } from "../../services/FirestoreManager";
 import * as ImagePicker from "expo-image-picker";
 import { isValidEmail } from "../../utils/Auth";
 import { useTranslation } from "react-i18next";
+
 interface ProfileScreenProps {
   onSaveChanges?: () => void;
 }
@@ -120,9 +121,9 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({ onSaveChanges }) => {
   };
 
   return (
-    <ScrollView className="flex-1 px-5 bg-white" testID="profile-screen">
+    <ScrollView className="flex-1 p-4 bg-white" testID="profile-screen">
       <TouchableOpacity
-        className="justify-center items-center mb-7.5 h-45"
+        className="justify-center items-center mb-6 h-48"
         onPress={pickImage}
         testID="profile-image-button"
       >
@@ -135,37 +136,33 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({ onSaveChanges }) => {
                 : require("../../../assets/images/profile.png")
           }
           testID="profile-image"
-          className="w-2/3 h-4/5 rounded-full border-4 border-gray-300"
+          className="w-48 h-48 rounded-full border-4 border-gray-300"
         />
       </TouchableOpacity>
-      <View className="my-5 bg-gray-100 rounded-lg h-20 px-2.5 py-1.25">
-        <Text className="text-base font-bold mb-1.25">
-          {t("settings.profile.name")}
-        </Text>
+      <View className="mb-4 bg-gray-100 rounded-lg px-4 py-2">
+        <Text className="font-bold mb-1">{t("settings.profile.name")}</Text>
         <TextInput
-          className="h-10 p-2.5 bg-white rounded"
+          className="h-10 px-3 bg-white rounded"
           placeholder={t("settings.profile.name")}
           value={name}
           onChangeText={setName}
         />
       </View>
-      <View className="mb-5 bg-gray-100 rounded-lg h-20  px-2.5 py-1.25">
-        <Text className="text-base font-bold mb-1.25">
-          {t("settings.profile.email")}
-        </Text>
+
+      <View className="mb-4 bg-gray-100 rounded-lg px-4 py-2">
+        <Text className="font-bold mb-1">{t("settings.profile.email")}</Text>
         <TextInput
-          className="h-10 p-2.5 bg-white rounded"
+          className="h-10 px-3 bg-white rounded"
           placeholder={t("settings.profile.email")}
           value={userEmail}
           onChangeText={setUserEmail}
         />
       </View>
-      <View className="mb-5 bg-gray-100 rounded-lg h-20 px-2.5 py-1.25">
-        <Text className="text-base font-bold mb-1.25">
-          {t("settings.profile.password")}
-        </Text>
+
+      <View className="mb-4 bg-gray-100 rounded-lg px-4 py-2">
+        <Text className="font-bold mb-1">{t("settings.profile.password")}</Text>
         <TextInput
-          className="h-10 p-2.5 bg-white rounded"
+          className="h-10 px-3 bg-white rounded"
           placeholder={t("settings.profile.password")}
           secureTextEntry={true}
           value={password}
@@ -174,11 +171,11 @@ const ProfileScreen: React.FC<ProfileScreenProps> = ({ onSaveChanges }) => {
       </View>
 
       <TouchableOpacity
-        className="bg-blue-500 rounded-lg py-3 px-5 items-center mt-2.5"
+        className="bg-primary-400 rounded-full py-3 px-6 items-center w-[70%] mx-auto"
         onPress={handleSaveChanges}
         testID="save-button"
       >
-        <Text className="text-white text-lg font-bold">
+        <Text className="text-white font-bold">
           {t("settings.profile.save-changes.button")}
         </Text>
       </TouchableOpacity>
